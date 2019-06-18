daf25673f494f96892a81a60742b29d499462151
frogconn <frogconn@gmail.com>
2019-06-18 15:27:47 +0700

    Implement GPIO.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import RPi.GPIO as GPIO
+from time import sleep
 from flask import (
     Flask,
     jsonify
@@ -10,6 +12,12 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Create a URL route in our application for "/"
 @app.route("/api/v1/users")
 def home():
+    GPIO.setwarnings(False)    # Ignore warning
+    GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+    GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.output(8, GPIO.HIGH) # Turn on
+    sleep(2)                  # Sleep for 1 second
+    GPIO.output(8, GPIO.LOW)  # Turn off
     return jsonify([{'name':'frogconn'}])
 
 # If we're running in stand alone mode, run the application
